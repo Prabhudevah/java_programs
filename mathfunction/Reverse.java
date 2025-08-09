e1876a15f4318b7d38d58f015969cfc76e1efce3
Prabhu <Prabhudevah003@gmail.com>
2025-08-09 19:34:04 +0530

    second

---
+++ mathfunction/Reverse.java
@@ -0,0 +1,11 @@
+package mathfunction;
+
+public class Reverse {
+    public static void main(String[] args) {
+     String str="chdeu";
+     StringBuffer reversed=new StringBuffer(str).reverse();
+     System.out.println(reversed);
+     
+    }
+    
+}
